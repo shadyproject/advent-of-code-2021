ee5badf8cd6a1d7bf8dda76a486d60a5b70e58d7
Chris Martin <shady@shadyproject.net>
2021-12-02 15:18:14 -0600

    use match instead of it let

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -6,24 +6,25 @@ use std::{
 
 fn main() {
     let data = "input.test";
-    let commands = read_input_file(data);
-    part_1(&commands);
+    match read_input_file(data) {
+        Ok(commands) => part_1(&commands),
+        Err(msg) => println!("{}", msg),
+    };
 }
 
 fn part_1(commands: &Vec<String>) {
     println!("Got {} commands", commands.len());
 }
 
-fn read_input_file(input: impl AsRef<Path>) -> Vec<String> {
+fn read_input_file(input: impl AsRef<Path>) -> Result<Vec<String>, &'static str> {
     if let Ok(file) = File::open(input) {
         let commands = BufReader::new(file)
             .lines()
             .map(|l| l.expect("Could not parse input."))
             .collect::<Vec<String>>();
 
-        return commands;
+        return Ok(commands);
     } else {
-        println!("ERRROR: Could not open input");
-        return vec![];
+        return Err("ERROR: Could not open input");
     }
 }
