e127c67a92b92c8ae3e9ad7d3290ef230f54ef62
Chris Martin <shady@shadyproject.net>
2021-12-01 21:22:22 -0600

    first pass

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut depth_change = 0;
+    let mut prev = i32::MIN;
+    if let Ok(file) = File::open("input") {
+        println!("Opened input");
+        let buffer = BufReader::new(file);
+
+        //read the first value
+        for line in buffer.lines() {
+            let curr = line.unwrap().parse::<i32>().unwrap();
+            if curr < prev {
+                depth_change += 1;
+            }
+
+            prev = curr;
+        }
+    } else {
+        println!("Error: Couldn't open file.")
+    }
+
+    println!("{} depth changes", depth_change);
 }
